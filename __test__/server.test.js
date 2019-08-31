c6043dc58b40fddc21fcef93a7ac7958ad3450bf
Pratiibh <pratiibhbassi@gmail.com>
2019-08-31 13:32:46 -0700

    final push

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,6 +1,38 @@
 'use strict';
 
-describe('Hello World Server', () => {
-    it('Can return Hello World!', () => {
+const { ApolloServer, gql } = require('apollo-server');
+const { InMemoryCache } = require('apollo-cache-inmemory');
+const { RestLink } = require('apollo-link-rest');
+const fetch = require('node-fetch').default;
+global.fetch = fetch;
+global.Headers = fetch.Headers;
 
-    });
+
+const server = new ApolloServer({
+  cache: new InMemoryCache(),
+  link: new RestLink({
+    uri: 'http://localhost:4000',
+  }),
+  fetch: fetch,
+});
+
+describe('Hello World Server', () => {
+  it('Can return Hello World!', () => {
+
+  });
+
+
+  it('Should get books', async () => {
+    const getBooks = gql`
+            query {
+                books {
+                    id
+                    title
+                    authors
+                }
+            }
+        `;
+    const response = await server.query({ query: getBooks });
+    expect(response.data.books[0].title).toBe('Apples');
+  });
+});
